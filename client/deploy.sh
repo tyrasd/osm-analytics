f144b648a0b0e37218154dd34d0c2c485d7c98f9
Martin Raifer <tyr.asd@gmail.com>
2016-04-11 15:01:02 +0200

    sign deploy pushes
    https://github.com/blog/2144-gpg-signature-verification

--- client/deploy.sh
+++ client/deploy.sh
@@ -7,7 +7,7 @@ npm run build
 cd static/
 git init
 git add .
-git commit -m "deploy"
+git commit -S -m "deploy"
 git push git@github.com:hotosm/osm-dat-frontend.git master:gh-pages --force
 rm -rf .git
 cd ..
