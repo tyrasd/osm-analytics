26126d8ca4211112b49da5818f16319ec716f3b3
Martin Raifer <tyr.asd@gmail.com>
2016-04-28 16:45:02 +0200

    use port 80 to fetch vector tiles from

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -1,4 +1,4 @@
 export default {
-  'vt-source': 'http://52.207.244.74:7778', // source of current vector tiles
-  'vt-hist-source': 'http://52.207.244.74:7778', // source of historic vector tiles for compare feature
+  'vt-source': 'http://52.207.244.74', // source of current vector tiles
+  'vt-hist-source': 'http://52.207.244.74', // source of historic vector tiles for compare feature
 }
